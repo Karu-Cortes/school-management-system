fc67721f4097448a57646415ca310850fb183f49
KarenCortes <karenj.cortesn@gmail.com>
2023-11-25 08:56:00 -0500

    initial project load

---
+++ src/test/java/org/adaschool/schoolmanagementsystem/SchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.schoolmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
