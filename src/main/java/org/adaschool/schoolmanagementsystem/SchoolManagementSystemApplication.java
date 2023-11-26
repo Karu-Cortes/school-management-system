b189634aeffca3e3d65b9a409915346ce96834be
KarenCortes <karenj.cortesn@gmail.com>
2023-11-25 19:28:06 -0500

    method creation and calling in main

--- src/main/java/org/adaschool/schoolmanagementsystem/SchoolManagementSystemApplication.java
+++ src/main/java/org/adaschool/schoolmanagementsystem/SchoolManagementSystemApplication.java
@@ -1,13 +1,56 @@
 package org.adaschool.schoolmanagementsystem;
 
-import org.springframework.boot.SpringApplication;
+import org.adaschool.schoolmanagementsystem.model.entity.Course;
+import org.adaschool.schoolmanagementsystem.model.entity.Student;
+import org.adaschool.schoolmanagementsystem.model.enums.Status;
+import org.adaschool.schoolmanagementsystem.model.enums.Version;
+import org.adaschool.schoolmanagementsystem.model.exception.CourseNotFoundException;
+import org.adaschool.schoolmanagementsystem.model.exception.StudentAlreadyInscribedException;
+import org.adaschool.schoolmanagementsystem.model.exception.StudentNotEnrolledInCourseException;
+import org.adaschool.schoolmanagementsystem.model.exception.StudentNotFoundException;
+import org.adaschool.schoolmanagementsystem.service.AcademicManager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class SchoolManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementSystemApplication.class, args);
-	}
+	public static void main(String[] args) throws StudentAlreadyInscribedException, StudentNotEnrolledInCourseException {
+		AcademicManager academicManager = new AcademicManager();
 
+		Student student = new Student(1, "Andrea", "Gonzalez", LocalDate.of(2000, 1, 15), Status.GRADUADO);
+		Student student2 = new Student(2, "Cecilia", "Torres", LocalDate.of(1993, 3, 21), Status.INACTIVO);
+		Course course = new Course(1, "Java", "Curso sobre fundamentos en Java SE", 3, Version.VERSION1);
+		Course course2 = new Course(2, "Base de datos", "Curso sobre fundamentos en Base de datos", 3, Version.VERSION2);
+
+		academicManager.enrollStudent(student);
+		academicManager.addCourse(course);
+		academicManager.enrollStudent(student2);
+		academicManager.addCourse(course2);
+
+		try {
+			academicManager.registerStudentCourse(student, 1);
+			academicManager.registerStudentCourse(student, 2);
+			academicManager.registerStudentCourse(student2, 1);
+			academicManager.registerStudentCourse(student2, 2);
+			academicManager.registerStudentCourse(student, 3); //disparar la excepciones
+
+		} catch (StudentAlreadyInscribedException |CourseNotFoundException  e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		try {
+			academicManager.unsubscribeStudentCourse(1, 1);
+			academicManager.unsubscribeStudentCourse(1, 2);
+			academicManager.unsubscribeStudentCourse(2, 1);
+			academicManager.unsubscribeStudentCourse(2, 2);
+			academicManager.unsubscribeStudentCourse(1, 3); //disparar la excepciones
+
+		} catch (StudentNotFoundException | StudentNotEnrolledInCourseException | CourseNotFoundException e) {
+			System.out.println("Error: " + e.getMessage());
+        }
+
+    }
 }
+
--- src/main/java/org/adaschool/schoolmanagementsystem/service/AcademicManager.java
+++ src/main/java/org/adaschool/schoolmanagementsystem/service/AcademicManager.java
@@ -5,6 +5,7 @@ import org.adaschool.schoolmanagementsystem.model.entity.Student;
 import org.adaschool.schoolmanagementsystem.model.exception.CourseNotFoundException;
 import org.adaschool.schoolmanagementsystem.model.exception.StudentAlreadyInscribedException;
 import org.adaschool.schoolmanagementsystem.model.exception.StudentNotEnrolledInCourseException;
+import org.adaschool.schoolmanagementsystem.model.exception.StudentNotFoundException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,7 +19,8 @@ public class AcademicManager implements IServiciosAcademicos{
     private List<Course> courses;
 
     //Creación de HashMap para mantener la lista de estudiantes inscritos en cada curso
-    private Map<Student, Course> enrolledStudents;
+
+    Map<Student, List<Course>> enrolledStudents;
 
     //Constructor
     /*en el cosntructor lo iniciamos con los tres atributos, donde Students y courses se instancian con ArrayList y enrolledStudents con HashMap
@@ -31,6 +33,12 @@ public class AcademicManager implements IServiciosAcademicos{
 
     // traemos los metodos implementados de la interfaz
     @Override
+
+    /* este metodo nos permite validar y crear un nuevo estudiante, verifiamos que
+    el estudiante no se encuentre en la colección students que contiene (contains) el estudiante student  y agregamos
+    con Students.add(student) esos datos.
+    * a la final se envia el mensaje de confirmación, donde por medio de los get traemos los datos de nombre y apellido*/
+
     public void enrollStudent(Student student) {
         if (students.contains(student)) {
             System.out.println("El estudiante ya se encuentra registrado");
@@ -52,29 +60,46 @@ public class AcademicManager implements IServiciosAcademicos{
 
     @Override
     public void registerStudentCourse(Student student, Integer courseId) throws StudentAlreadyInscribedException, CourseNotFoundException {
-        if (enrolledStudents.containsKey(student)) {
-            throw new StudentAlreadyInscribedException("El estudiante ya está inscrito en un curso");
-        }
-
 
         Course course = findCourseById(courseId);
-
 
         if (course == null) {
             System.out.println("No se encontró el curso con el ID ingresado");
             return;
         }
 
+        List<Course> studentCourses = enrolledStudents.get(student);
+        if (studentCourses != null && studentCourses.contains(course)) {
+            throw new StudentAlreadyInscribedException("El estudiante ya está inscrito en el curso");
+        }
 
-        enrolledStudents.put(student, course);
+        if (studentCourses == null) {
+            studentCourses = new ArrayList<>();
+            enrolledStudents.put(student, studentCourses);
+        }
+
+        studentCourses.add(course);
+
         System.out.println("El estudiante " + student.getName() + " " + student.getFirstName() +
                 " se ha inscrito en el curso " + course.getName() + " de forma exitosa");
 
     }
 
     @Override
-    public void unsubscribeStudentCourse(Integer studentId, Integer courseId) throws StudentNotEnrolledInCourseException {
+    public void unsubscribeStudentCourse(Integer studentId, Integer courseId) throws StudentNotEnrolledInCourseException, CourseNotFoundException, StudentNotFoundException {
+        Student student = findStudentById(studentId);
+        Course course = findCourseById(courseId);
 
+        List<Course> studentCourses = enrolledStudents.get(student);
+
+        if (studentCourses == null || !studentCourses.contains(course)) {
+            throw new StudentNotEnrolledInCourseException("El estudiante no está inscrito en el curso");
+        }
+
+        studentCourses.remove(course);
+
+        System.out.println("El estudiante " + student.getName() + " " + student.getFirstName() +
+                " se ha desinscrito del curso " + course.getName() + " de forma exitosa");
     }
 
     private Course findCourseById(Integer courseId) throws CourseNotFoundException {
@@ -86,5 +111,13 @@ public class AcademicManager implements IServiciosAcademicos{
         throw new CourseNotFoundException("No se encontró el curso con el ID ingresado");
     }
 
+    private Student findStudentById(Integer studentId) throws StudentNotFoundException {
+        for (Student student : students) {
+            if (student.getId() == studentId) {
+                return student;
+            }
+        }
+        throw new StudentNotFoundException("No se encontró el estudiante con el ID ingresado");
+    }
 }
 
