b7d4451e0943a4234bfae551792f7049a84539ff
KarenCortes <karenj.cortesn@gmail.com>
2023-11-25 19:28:06 -0500

    method creation and calling in main

---
+++ src/main/java/org/adaschool/schoolmanagementsystem/model/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package org.adaschool.schoolmanagementsystem.model.exception;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/adaschool/schoolmanagementsystem/service/IServiciosAcademicos.java
+++ src/main/java/org/adaschool/schoolmanagementsystem/service/IServiciosAcademicos.java
@@ -5,6 +5,7 @@ import org.adaschool.schoolmanagementsystem.model.entity.Student;
 import org.adaschool.schoolmanagementsystem.model.exception.CourseNotFoundException;
 import org.adaschool.schoolmanagementsystem.model.exception.StudentAlreadyInscribedException;
 import org.adaschool.schoolmanagementsystem.model.exception.StudentNotEnrolledInCourseException;
+import org.adaschool.schoolmanagementsystem.model.exception.StudentNotFoundException;
 
 public interface IServiciosAcademicos {
 
@@ -15,5 +16,5 @@ public interface IServiciosAcademicos {
     //los últimos dos metodos, deben tener excepciones
     void registerStudentCourse(Student student, Integer courseId) throws StudentAlreadyInscribedException, CourseNotFoundException;
 
-    void unsubscribeStudentCourse(Integer studentId, Integer courseId) throws StudentNotEnrolledInCourseException;
+    void unsubscribeStudentCourse(Integer studentId, Integer courseId) throws StudentNotEnrolledInCourseException, CourseNotFoundException, StudentNotFoundException;
 }
